961dbab1df35e428f968f7f41db04cd0f9499b78
joseph townshend <joetownshend@mac.com>
2019-09-22 16:17:29 +0100

    feat: canMakeBank with Bank class setup

---
+++ src/test/java/bankTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+public class bankTest {
+
+    @Test
+    public void canMakeBank() {
+        new Bank();
+    }
+
+}
