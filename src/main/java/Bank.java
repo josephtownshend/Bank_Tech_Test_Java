e8f49a6a876f40b3351e612ea8572a2b059406fa
joseph townshend <joetownshend@mac.com>
2019-09-23 10:34:39 +0100

    feat: transactionHistory Array setup as ArrayList

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -1,6 +1,10 @@
+import java.util.ArrayList;
+
+
 public class Bank {
 
     private int balance = 0;
+    ArrayList<Integer> transactionHistory = new ArrayList<Integer>(0)
 
     public int balance() {
         return balance;
